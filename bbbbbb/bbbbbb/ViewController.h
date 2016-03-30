878d25f915519275f635bf0f4319ca34c68b524a
Coke <365754921@qq.com>
2016-03-30 12:26:17 +0800

    上传

---
+++ bbbbbb/bbbbbb/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  bbbbbb
+//
+//  Created by Coke on 16/3/30.
+//  Copyright © 2016年 Coke26. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ bbbbbb/bbbbbb/ViewController.m
@@ -0,0 +1,34 @@
+//
+//  ViewController.m
+//  bbbbbb
+//
+//  Created by Coke on 16/3/30.
+//  Copyright © 2016年 Coke26. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    NSLog(@"bbbbb");
+    
+    NSLog(@"ccccc");
+    
+    NSLog(@"我是self ");
+
+    NSLog(@"我是经理");
+
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
